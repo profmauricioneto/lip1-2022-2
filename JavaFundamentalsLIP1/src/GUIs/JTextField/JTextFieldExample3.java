b3e84d7df23d721f9da766a8da4146c441664e06
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2022-09-27 07:20:45 -0300

    feat: example of jtextfield

--- JavaFundamentalsLIP1/src/GUIs/JTextField/JTextFieldExample3.java
+++ JavaFundamentalsLIP1/src/GUIs/JTextField/JTextFieldExample3.java
@@ -7,13 +7,15 @@ package GUIs.JTextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+
 /**
  *
  * @author maumneto
  */
-public class JTextFieldExample {
+public class JTextFieldExample3 {
+
     public static void main(String[] args) {
-        
+
         // create elements of the GUI
         JFrame frame = new JFrame("JTextFieldExample");
         JLabel firstname_label = new JLabel("First Name: ");
@@ -23,8 +25,7 @@ public class JTextFieldExample {
         JButton buttonShow = new JButton("Click Here");
         JLabel result_label = new JLabel();
         JButton buttonClear = new JButton("Clear");
-        
-        
+
         // adding these elements in the frame
         frame.getContentPane().add(firstname_label);
         frame.getContentPane().add(lastname_label);
@@ -33,10 +34,10 @@ public class JTextFieldExample {
         frame.getContentPane().add(buttonShow);
         frame.getContentPane().add(result_label);
         frame.getContentPane().add(buttonClear);
-        
+
         // disabling the auto-layout
         frame.setLayout(null);
-        
+
         // set the position of the elements
         firstname_label.setBounds(20, 20, 100, 20);
         firstname_textfield.setBounds(120, 20, 300, 20);
@@ -45,17 +46,20 @@ public class JTextFieldExample {
         buttonShow.setBounds(150, 90, 100, 20);
         buttonClear.setBounds(280, 90, 100, 20);
         result_label.setBounds(20, 120, 300, 20);
-        
+
         // adding an action to the button (when the button clicks)
-        buttonShow.addActionListener(new ActionListener(){
+        buttonShow.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String firstname = firstname_textfield.getText().toUpperCase();
                 String lastname = lastname_textfield.getText().toUpperCase();
-                result_label.setText("Hello, Sr(a). " + firstname + " " + lastname);
+                if (firstname.isEmpty() || lastname.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Entrada vazia!", "Error", 1);
+                } else {
+                    result_label.setText("Hello, Sr(a). " + firstname + " " + lastname);
+                }
             }
         });
-        
         // action of clear button
         buttonClear.addActionListener(new ActionListener() {
             @Override
@@ -64,12 +68,12 @@ public class JTextFieldExample {
                 lastname_textfield.setText("");
                 result_label.setText("");
             }
-        }); 
-        
+        });
+
         // configurate the frame
         frame.setSize(500, 300);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
     }
 }
